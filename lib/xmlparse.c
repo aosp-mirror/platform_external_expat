7abdc8dbc26ca56f4af1fffda96731b6ab9a135d
Sadaf Ebrahimi <sadafebrahimi@google.com>
2022-06-03 03:40:21 +0000

    Prevent integer overflow in function doProlog
    Bug: http://b/221256678
    Change-Id: I6fe381103f4eb287726d1ccb5bfec99db160ffe4
    (cherry picked from commit 257f1d3777240016d3ccd74a61cd7d0e0efcaae3)
    Merged-In: I6fe381103f4eb287726d1ccb5bfec99db160ffe4

--- lib/xmlparse.c
+++ lib/xmlparse.c
@@ -5210,23 +5210,29 @@ doProlog(XML_Parser parser,
       if (dtd->in_eldecl) {
         ELEMENT_TYPE *el;
         const XML_Char *name;
-        int nameLen;
-        const char *nxt = (quant == XML_CQUANT_NONE
-                           ? next
-                           : next - enc->minBytesPerChar);
+        size_t nameLen;
+        const char *nxt
+            = (quant == XML_CQUANT_NONE ? next : next - enc->minBytesPerChar);
         int myindex = nextScaffoldPart(parser);
         if (myindex < 0)
           return XML_ERROR_NO_MEMORY;
         dtd->scaffold[myindex].type = XML_CTYPE_NAME;
         dtd->scaffold[myindex].quant = quant;
         el = getElementType(parser, enc, s, nxt);
-        if (!el)
+        if (! el)
           return XML_ERROR_NO_MEMORY;
         name = el->name;
         dtd->scaffold[myindex].name = name;
         nameLen = 0;
-        for (; name[nameLen++]; );
-        dtd->contentStringLen +=  nameLen;
+        for (; name[nameLen++];)
+          ;
+
+        /* Detect and prevent integer overflow */
+        if (nameLen > UINT_MAX - dtd->contentStringLen) {
+          return XML_ERROR_NO_MEMORY;
+        }
+
+        dtd->contentStringLen += (unsigned)nameLen;
         if (parser->m_elementDeclHandler)
           handleDefault = XML_FALSE;
       }
